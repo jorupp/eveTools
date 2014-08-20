939491adaaeaeaf943424b14ae357bed7e2c115a
jorupp <jorupp@gmail.com>
2014-08-20 16:26:15 -0500

    Industry stuff isn't updated for Cryus :(

---
+++ EveTools.Web/Controllers/IndustryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EveAI.Live;
+using EveTools.Domain;
+
+namespace EveTools.Web.Controllers
+{
+    public class IndustryController : Controller
+    {
+        private readonly IApiKeyRepository _apiKeyRepository;
+
+        public IndustryController(IApiKeyRepository apiKeyRepository)
+        {
+            _apiKeyRepository = apiKeyRepository;
+        }
+
+        // GET: Industry
+        public ActionResult Index(string id, bool isCorp, int characterId)
+        {
+            var key = _apiKeyRepository.GetById(id);
+            if (null == key)
+                return HttpNotFound();
+            var api = new EveApi(key.keyId, key.vCode, characterId);
+            var jobs = isCorp ? api.GetCorporationIndustryJobs() : api.GetCharacterIndustryJobs();
+
+            return Content(jobs.Count.ToString());
+        }
+    }
+}
